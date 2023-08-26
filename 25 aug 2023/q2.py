86284348808b9b4141046b60066b249d789a526e
priyanshi232 <priyanshi6212@gmail.com>
2023-08-26 13:07:03 +0530

    Revert "codes assignment"
    This reverts commit d6bb3eda992f58eb06893fcd803a867d8c8eaaf7.

--- 25 aug 2023/q2.py
+++
@@ -1,7 +0,0 @@
-try:
-    num = float(input("Enter a number: "))
-    print(f"You entered: {num}")
-except KeyboardInterrupt:
-    print("\nUser canceled the input.")
-except ValueError:
-    print("Error: Please enter a valid number.")
